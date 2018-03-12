fd19b6eb9f947d409940912e240f4ba8e898543f
Unknown <wndtanaka@gmail.com>
2018-03-12 12:08:19 +1100

    Week 6

--- Assets/Scripts/SignUp.cs
+++ Assets/Scripts/SignUp.cs
@@ -9,19 +9,61 @@ public class SignUp : MonoBehaviour
     public InputField newUsername;
     public InputField newEmail;
     public InputField newPassword;
-    public InputField confirmPassword;
 
-    //UserDetails user;
+    public Text notify;
 
-    //public void NewSignUp()
-    //{
-    //    if (user.usernameList.Contains(newUsername.text))
-    //    {
-    //        Debug.Log("Nah Mate");
-    //    }
-    //}
+    void Start()
+    {
+        notify.enabled = false;
+    }
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            StartCoroutine(CreateAccount(newUsername.text, newEmail.text, newPassword.text));
+            StartCoroutine(ShowNotification());
+        }
+    }
+
+    public void CreateAccountButton()
+    {
+        StartCoroutine(CreateAccount(newUsername.text, newEmail.text, newPassword.text));
+        StartCoroutine(ShowNotification());
+    }
+
+    IEnumerator CreateAccount(string username, string email, string password)
+    {
+        string createUserURL = "http://localhost/loginsystem/insertuser.php";
+        WWWForm user = new WWWForm();
+        user.AddField("username_Post", username);
+        user.AddField("email_Post", email);
+        user.AddField("password_Post", password);
+        WWW www = new WWW(createUserURL, user);
+        yield return www;
+        if (www.text == "Create First User" || www.text == username + "Created User")
+        {
+            notify.text = "Your account is created";
+        }
+        else if (www.text == username + "User Already Exists")
+        {
+            notify.text = "Username is taken, please choose another username";
+        }
+        else if (www.text == username + "Email Already Exists")
+        {
+            notify.text = email + " has been registered.";
+        }
+    }
+
     public void Cancel()
     {
         SceneManager.LoadScene("Login");
+    }
+
+    IEnumerator ShowNotification()
+    {
+        notify.enabled = true;
+        yield return new WaitForSeconds(2f);
+        notify.enabled = false;
+
     }
 }
