b1febbd7c8e9c1132e8e3b4f676c8c68629accf5
Unknown <wndtanaka@gmail.com>
2018-05-17 16:01:07 +1000

    try to create inventory UI

---
+++ Assets/Scripts/InventoryUI.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InventoryUI : MonoBehaviour
+{
+    private RectTransform inventoryRect;
+
+    private float inventoryWidth, inventoryHeight;
+
+    public int slots;
+    public int rows;
+
+    public float slotPaddingLeft, slotPaddingTop;
+
+    public float slotSize;
+
+    public GameObject slotPrefab;
+
+    private List<GameObject> allSlots;
+    // Use this for initialization
+    void Start()
+    {
+        CreateLayout();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void CreateLayout()
+    {
+        allSlots = new List<GameObject>();
+
+        inventoryWidth = (slots / rows) * (slotSize + slotPaddingLeft) ;
+        inventoryHeight = rows * (slotSize + slotPaddingTop) ;
+
+        inventoryRect = GetComponent<RectTransform>();
+
+        inventoryRect.SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, inventoryWidth);
+        inventoryRect.SetSizeWithCurrentAnchors(RectTransform.Axis.Vertical, inventoryHeight );
+
+        int columns = slots / rows;
+
+        for (int y = 0; y < rows; y++)
+        {
+            for (int x = 0; x < columns; x++)
+            {
+                GameObject newSlot = Instantiate(slotPrefab);
+
+                RectTransform slotRect = newSlot.GetComponent<RectTransform>();
+
+                newSlot.name = "Slot";
+                newSlot.transform.SetParent(this.transform.parent);
+
+                slotRect.localPosition = inventoryRect.localPosition + new Vector3(slotPaddingLeft * (x + 1) + (slotSize * x), -slotPaddingTop * (y + 1) - (slotSize * y));
+                slotRect.SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, slotSize);
+                slotRect.SetSizeWithCurrentAnchors(RectTransform.Axis.Vertical, slotSize);
+
+                allSlots.Add(newSlot);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/ItemList.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum ItemType { MANA, HEALTH };
+
+public class ItemList : MonoBehaviour
+{
+    public ItemType type;
+    public Sprite spriteNeutral;
+    public Sprite spriteHighlighted;
+
+    public int maxSize;
+
+    public void Use()
+    {
+        switch (type)
+        {
+            case ItemType.MANA:
+                Debug.Log("Mana Potion Used.");
+                break;
+            case ItemType.HEALTH:
+                Debug.Log("Health Potion Used.");
+                break;
+            default:
+                break;
+        }
+    }
+}
