33470ac0b399666501d01a301e501d8151071c61
Unknown <wndtanaka@gmail.com>
2018-05-22 11:57:45 +1000

    character class toggling

---
+++ Assets/CharacterDetails.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CharacterDetails : MonoBehaviour
+{
+    public string[] charClass = new string[] { "Warrior", "Rogue", "Mage", "Priest" };
+
+    int index = 0;
+    public Text charClassText;
+
+    void Start()
+    { 
+        charClassText.text = charClass[0];
+    }
+
+    public void TogglingClass(int amount)
+    {
+        index += amount;
+        if (index <= 0)
+        {
+            index = charClass.Length - 1;
+            charClassText.text = charClass[index];
+        }
+        else if (index < charClass.Length)
+        {
+            charClassText.text = charClass[index];
+        }
+        else if (index >= charClass.Length)
+        {
+            index = 0;
+            charClassText.text = charClass[index];
+        }
+    }
+}
--- Assets/Scripts/CharacterCreation.cs
+++ Assets/Scripts/CharacterCreation.cs
@@ -20,14 +20,14 @@ public class CharacterCreation : MonoBehaviour
 
     }
 
-    IEnumerator CharacterCreation()
-    {
-        string loginURL = "http://localhost/loginsystem/CharacterCreation.php";
-        WWWForm loginForm = new WWWForm();
-        loginForm.AddField("username_Post", username.text);
-        loginForm.AddField("password_Post", password.text);
-        WWW www = new WWW(loginURL, loginForm);
-        yield return www;
-        Debug.Log(www.text);
-    }
+    //IEnumerator CharacterCreation()
+    //{
+    //    string loginURL = "http://localhost/loginsystem/CharacterCreation.php";
+    //    WWWForm loginForm = new WWWForm();
+    //    loginForm.AddField("username_Post", username.text);
+    //    loginForm.AddField("password_Post", password.text);
+    //    WWW www = new WWW(loginURL, loginForm);
+    //    yield return www;
+    //    Debug.Log(www.text);
+    //}
 }
